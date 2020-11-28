f2840db1ca3e05197e769b2663650c8441f87b45
nikitha <nikitha.devan.23@gmail.com>
2020-11-28 15:02:25 -0600

    Fixed request mapping at the controller level

--- escuela-api/src/main/java/com/escuela/api/controllers/RegistrationController.java
+++ escuela-api/src/main/java/com/escuela/api/controllers/RegistrationController.java
@@ -3,16 +3,20 @@ package com.escuela.api.controllers;
 import com.escuela.api.db.models.Registration;
 import com.escuela.api.jpa.repositories.RegistrationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/register")
+@RestController
+@RequestMapping("/register")
 public class RegistrationController {
 
     @Autowired
     RegistrationRepository registrationRepository;
 
+    @GetMapping("/findall")
     public List<Registration> getAllRegisteredUsers(){
         return registrationRepository.findAll();
     }
