89fa3475d127bf37c10230e5f17dbdb71eaa836f
Escuela Technologies <escuela.tech@gmail.com>
2020-12-01 19:55:17 -0600

    Updated the method .

---
+++ escuela-api/src/main/java/com/escuela/api/service/implementations/VideoServiceImpl.java
@@ -0,0 +1,30 @@
+package com.escuela.api.service.implementations;
+
+import com.escuela.api.db.models.VideoDetails;
+import com.escuela.api.jpa.repositories.VideoDetailsRepository;
+import com.escuela.api.service.interfaces.VideoService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service(value = "videoservice")
+public class VideoServiceImpl implements VideoService{
+
+
+    private static native void registerNatives();
+
+    final VideoDetailsRepository videoDetailsRepository;
+
+    public VideoServiceImpl(VideoDetailsRepository videoDetailsRepository) {
+        this.videoDetailsRepository = videoDetailsRepository;
+    }
+
+    @Override
+    public List<VideoDetails> getAllVideos() {
+        return videoDetailsRepository.findAll();
+    }
+
+}
