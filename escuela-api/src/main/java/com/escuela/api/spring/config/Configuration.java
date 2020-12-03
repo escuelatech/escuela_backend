f7038415cc8fae1f084e7c87291ba95947e9e0ec
Escuela Technologies <escuela.tech@gmail.com>
2020-12-03 16:50:18 -0600

    Added cors configuration

---
+++ escuela-api/src/main/java/com/escuela/api/spring/config/Configuration.java
@@ -0,0 +1,25 @@
+package com.escuela.api.spring.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+        @Bean
+        public FilterRegistrationBean corsFilter() {
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowCredentials(true);
+            config.addAllowedOrigin("http://localhost:3000");
+            config.addAllowedHeader("*");
+            config.addAllowedMethod("*");
+            source.registerCorsConfiguration("/**", config);
+            FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+            bean.setOrder(0);
+            return bean;
+        }
+}
