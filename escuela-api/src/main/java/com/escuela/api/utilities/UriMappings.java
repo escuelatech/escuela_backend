6d3e47a140bc4d3201787a77b3f5f70ebbce6e63
Escuela Technologies <escuela.tech@gmail.com>
2020-12-04 15:48:35 -0600

    URI mapping class added

---
+++ escuela-api/src/main/java/com/escuela/api/utilities/UriMappings.java
@@ -0,0 +1,9 @@
+package com.escuela.api.utilities;
+
+public class UriMappings {
+
+    public static final String REGISTER="/register";
+    public static final String FIND_ALL_REGISTERED_USERS="/findall";
+    public static final String CREATE_USER="/adduser";
+
+}
