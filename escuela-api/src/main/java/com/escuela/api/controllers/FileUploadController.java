4be3da126e5862ef2e3a003d9ac80c0d8b8c925e
Escuela Technologies <escuela.tech@gmail.com>
2021-01-11 23:23:51 -0600

    Read bytes from multipart file

--- escuela-api/src/main/java/com/escuela/api/controllers/FileUploadController.java
+++ escuela-api/src/main/java/com/escuela/api/controllers/FileUploadController.java
@@ -4,6 +4,8 @@ package com.escuela.api.controllers;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/upload")
@@ -11,6 +13,11 @@ public class FileUploadController {
 
     @PostMapping("/fileupload")
     public String fileUpload(@RequestParam("file")MultipartFile multipartFile){
+        try {
+            byte[] bytes = multipartFile.getBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return String.format("File is uploaded successfully", multipartFile.getOriginalFilename());
     }
 }
