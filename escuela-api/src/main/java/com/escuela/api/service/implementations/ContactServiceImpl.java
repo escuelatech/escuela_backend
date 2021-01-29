bd5d7284ed5a9a974ee9dbead5e53a35b781fc93
skramath <s.koiloth.ramath@accenture.com>
2021-01-30 00:42:38 +0530

    changed email sending services for contactus flow

--- escuela-api/src/main/java/com/escuela/api/service/implementations/ContactServiceImpl.java
+++ escuela-api/src/main/java/com/escuela/api/service/implementations/ContactServiceImpl.java
@@ -4,20 +4,46 @@ package com.escuela.api.service.implementations;
 import com.escuela.api.db.models.Contact;
 import com.escuela.api.jpa.repositories.ContactRepository;
 import com.escuela.api.service.interfaces.ContactService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.escuela.api.service.interfaces.EmailService;
+import com.escuela.api.ui.models.Mail;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service(value = "contactservice")
 public class ContactServiceImpl implements ContactService {
     final
     ContactRepository contactRepository;
 
-    public ContactServiceImpl(ContactRepository contactRepository) {
+    final
+    EmailService emailService;
+
+    public ContactServiceImpl(ContactRepository contactRepository,EmailService emailService) {
         this.contactRepository = contactRepository;
+        this.emailService = emailService;
     }
 
     public  String contactUs(Contact newContact){
+        emailService.sendEmails(getMailDetails(newContact));
         contactRepository.save(newContact);
         return "successfully submitted your contact";
+    }
+
+    private Mail getMailDetails(Contact newContact) {
+        Mail mail = new Mail();
+        mail.setEmail(newContact.getEmail());
+        mail.setMessage(newContact.getMessage());
+        mail.setSubject("[Esquela-Tech] Customer Enquiry Received ");
+        mail.setFrom("ESQUELA-TECH");
+        mail.setAck("Thanks for your enquiry, " +
+                "We are happy to help you. Our team will be in touch with you soon.");
+        mail.setTemplateName("/email_templates/contactus_mail.ftl");
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("message", mail);
+        model.put("greeting", "Hello " + newContact.getLastName() + " " + newContact.getFirstName() + ",");
+        model.put("baseUrl", "www.esquela-tech.com"); //not used
+        mail.setTemplateModel(model);
+        return mail;
     }
 }
--- escuela-api/src/main/java/com/escuela/api/service/implementations/EmailServiceImpl.java
+++ escuela-api/src/main/java/com/escuela/api/service/implementations/EmailServiceImpl.java
@@ -29,24 +29,13 @@ public class EmailServiceImpl implements EmailService {
 
     @Deprecated
     public void sendEmails(Mail mValue){
-         Mail mail = new Mail();
-            mail.setEmail(mValue.getEmail());
-            mail.setMessage(mValue.getMessage());
-            mail.setSubject("[Esquela-Tech] Customer Enquiry Received ");
-            mail.setFrom("ESQUELA-TECH");
-            mail.setAck("Thanks for your enquiry, " +
-            "We are happy to help you. Our team will be in touch with you soon.");
-            Map<String, Object> model = new HashMap<String,Object>();
-            model.put("message",mail);
-            model.put("greeting", "Hello "+mValue.getLastName() +" "+mValue.getFirstName()+",");
-            model.put("baseUrl", "www.esquela-tech.com"); //not used
-            try {
-                email.sendInDividualEmail(mail.getEmail(), mail.getSubject(),
-                        templateService.getData(model, "/email_template/contactus_mail.ftl"));
-                System.err.println(String.format("Contact Email sent from: %s", mail.getFrom()));
-            } catch (Exception e) {
+         try {
+                email.sendInDividualEmail(mValue.getEmail(), mValue.getSubject(),
+                        templateService.getData(mValue.getTemplateModel(), mValue.getTemplateName()));
+                System.err.println(String.format("Mail sent with Sub: %s, From:%s", mValue.getSubject(),mValue.getFrom()));
+         } catch (Exception e) {
                 e.printStackTrace(); //todo hANDLE eXCEPTION
-            }
+         }
     }
 
 
