befd27259309ad143985069a55af4e2447117a66
Escuela Technologies <escuela.tech@gmail.com>
2021-01-28 21:16:22 -0600

    Changed the mapping value to something meaningful

--- escuela-api/src/main/java/com/escuela/api/controllers/RecruiterProfileController.java
+++ escuela-api/src/main/java/com/escuela/api/controllers/RecruiterProfileController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/company")
+@RequestMapping("/recruiter")
 public class RecruiterProfileController {
 
 
