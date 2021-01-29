8994d577151d75f9fd855cc69b4b4bec1e0be287
Escuela Technologies <escuela.tech@gmail.com>
2021-01-28 21:13:19 -0600

    Created Recruiter profile controller

---
+++ escuela-api/src/main/java/com/escuela/api/controllers/RecruiterProfileController.java
@@ -0,0 +1,14 @@
+package com.escuela.api.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/company")
+public class RecruiterProfileController {
+
+
+
+}
