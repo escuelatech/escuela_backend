d5f9a9d0eff7557e3a152474f2e64f279a74e148
Shyam Ramath <ameya@Shyams-MBP.attlocal.net>
2021-01-29 15:42:16 -0600

    deleted the urimapping class, it was giving complile errors

--- escuela-api/src/main/java/com/escuela/api/utilities/ApiMappings.java
+++ escuela-api/src/main/java/com/escuela/api/utilities/ApiMappings.java
@@ -1,9 +1,8 @@
 package com.escuela.api.utilities;
 
-public class URIMAPPINGS {
+public class ApiMappings {
 
     public static final String REGISTER="/register";
     public static final String FIND_ALL_REGISTERED_USERS="/findall";
     public static final String CREATE_USER="/adduser";
-
 }
--- escuela-api/src/main/java/com/escuela/api/utilities/UriMappings.java
+++
@@ -1,9 +0,0 @@
-package com.escuela.api.utilities;
-
-public class URIMAPPINGS {
-
-    public static final String REGISTER="/register";
-    public static final String FIND_ALL_REGISTERED_USERS="/findall";
-    public static final String CREATE_USER="/adduser";
-
-}
